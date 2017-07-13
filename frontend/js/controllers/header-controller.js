a39b7b5957dd111b50f9099e8bf662b71615c968
snehalthakare <snehal.thakare@wohlig.com>
2017-07-13 17:40:20 +0530

    division header and faq

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -4,5 +4,39 @@ myApp.controller('headerCtrl', function ($scope, TemplateService) {
         $(window).scrollTop(0);
     });
     $.fancybox.close(true);
+    console.log("in header ctrl");
     $.jStorage.set('FirstTime');
+    $scope.division = [{
+        name: "uro veener world",
+        id: "uroveenerworld",
+    }, {
+        name: "wood & mouldings",
+        id: "woodmouldings",
+    }, {
+        name: " bath world",
+        id: "bathworld",
+    }, {
+        name: " gala hardware worlds",
+        id: "galahardwareworlds",
+    }, {
+        name: " gala stone worlds",
+        id: "galastoneworlds",      
+    }, {
+        name: " gala furniture worlds",
+        id: "galafurnitureworlds",      
+    }, {
+        name: "euro lighting world",
+        id: "eurolightingworld",    
+    }, {
+        name: "gala drapes world",
+        id: "galadrapesworld",      
+    }, {
+        name: "gala kitchen world",
+        id: "galakitchenworld",       
+    }, {
+        name: "mahavir corporation (India)",
+        id: "mahavircorporation",
+    }];
+//  $scope.homecategory = _.chunk($scope.category, 4);
+
 });
