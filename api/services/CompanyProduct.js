a78e50761df9e92d853534b3133a3357fd6ac97b
Chintan <chintan@wohlig.com>
2017-11-07 12:48:14 +0530

    changes for the google login

--- api/services/CompanyProduct.js
+++ api/services/CompanyProduct.js
@@ -26,11 +26,11 @@ var schema = new Schema({
         ref: 'CompanyCategory',
         index: true
     },
-     company: {
+    company: {
         type: Schema.Types.ObjectId,
         ref: 'Company',
         index: true
-    },//filter
+    }, //filter
 
     enquiry: [{
         subject: {
@@ -62,7 +62,7 @@ module.exports = mongoose.model('CompanyProduct', schema);
 // var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "companyCategory companyCategory.company", "companyCategory companyCategory.company","company", "company","createdAt", "desc"));
 
 
-var exports = _.cloneDeep(require("sails-wohlig-service")(schema,"company", "company","companyCategory" ,"companyCategory","createdAt", "desc"));
+var exports = _.cloneDeep(require("sails-wohlig-service")(schema, "company", "company", "companyCategory", "companyCategory", "createdAt", "desc"));
 
 
 var model = {
@@ -269,111 +269,113 @@ var model = {
             }
         })
     },
-//     search:function (data, callback){
-//        if (data.count) {
-//            var maxCount = data.count;
-//        } else {
-//            var maxCount = Config.maxRow;
-//        }
-//        var maxRow = maxCount
-//        var page = 1;
-//        if (data.page) {
-//            page = data.page;
-//        }
-//        var field = data.field;
-//        var options = {
-//            field: data.field,
-//            filters: {
-//                keyword: {
-//                    fields: ['name'],
-//                    term: data.keyword
-//                }
-//            },
-//            sort: {
-//                desc: 'createdAt'
-//            },
-//            start: (page - 1) * maxRow,
-//            count: maxRow
-//        };
-//        var match={};
-//        if(data.company){
-//            console.log("if");
-//                       match={company: data.company}
-//        }else{
-//            console.log("else");
-//                   match={}
-//        }
-//        console.log("match---",match);
-//        CompanyProduct.find(match)
-//            .order(options)
-//            .keyword(options)
-//            .page(options,
-//                function (err, found) {
-                  
-//                    if (err) {
-//                        callback(err, null);
-//                    } else if (found) {
-//                        callback(null, found);
-//                    } else {
-//                        callback("Invalid data", null);
-//                    }
-//                });
+    //     search:function (data, callback){
+    //        if (data.count) {
+    //            var maxCount = data.count;
+    //        } else {
+    //            var maxCount = Config.maxRow;
+    //        }
+    //        var maxRow = maxCount
+    //        var page = 1;
+    //        if (data.page) {
+    //            page = data.page;
+    //        }
+    //        var field = data.field;
+    //        var options = {
+    //            field: data.field,
+    //            filters: {
+    //                keyword: {
+    //                    fields: ['name'],
+    //                    term: data.keyword
+    //                }
+    //            },
+    //            sort: {
+    //                desc: 'createdAt'
+    //            },
+    //            start: (page - 1) * maxRow,
+    //            count: maxRow
+    //        };
+    //        var match={};
+    //        if(data.company){
+    //            console.log("if");
+    //                       match={company: data.company}
+    //        }else{
+    //            console.log("else");
+    //                   match={}
+    //        }
+    //        console.log("match---",match);
+    //        CompanyProduct.find(match)
+    //            .order(options)
+    //            .keyword(options)
+    //            .page(options,
+    //                function (err, found) {
 
-// },
+    //                    if (err) {
+    //                        callback(err, null);
+    //                    } else if (found) {
+    //                        callback(null, found);
+    //                    } else {
+    //                        callback("Invalid data", null);
+    //                    }
+    //                });
 
-search:function (data, callback){
-       if (data.count) {
-           var maxCount = data.count;
-       } else {
-           var maxCount = Config.maxRow;
-       }
-       var maxRow = maxCount
-       var page = 1;
-       if (data.page) {
-           page = data.page;
-       }
-       var field = data.field;
-       var options = {
-           field: data.field,
-           filters: {
-               keyword: {
-                   fields: ['name'],
-                   term: data.keyword
-               }
-           },
-           sort: {
-               desc: 'createdAt'
-           },
-           start: (page - 1) * maxRow,
-           count: maxRow
-       };
- 
-       console.log("sssssssssssss",data);
-       
-       var match={};
-       if(!_.isEmpty(data.filter)){
-                      match={company: data.filter.company}
-                      
-       }else{
-                  match={}
-                  
-       }
-       console.log("match---",match);
-       CompanyProduct.find(match)
-           .order(options)
-           .keyword(options)
-           .page(options,
-               function (err, found) {
-                   if (err) {
-                       callback(err, null);
-                   } else if (found) {
-                       console.log("found",found);
-                       callback(null, found);
-                   } else {
-                       callback("Invalid data", null);
-                   }
-               });
+    // },
 
-}
+    search: function (data, callback) {
+        if (data.count) {
+            var maxCount = data.count;
+        } else {
+            var maxCount = Config.maxRow;
+        }
+        var maxRow = maxCount
+        var page = 1;
+        if (data.page) {
+            page = data.page;
+        }
+        var field = data.field;
+        var options = {
+            field: data.field,
+            filters: {
+                keyword: {
+                    fields: ['name'],
+                    term: data.keyword
+                }
+            },
+            sort: {
+                desc: 'createdAt'
+            },
+            start: (page - 1) * maxRow,
+            count: maxRow
+        };
+
+        console.log("sssssssssssss", data);
+
+        var match = {};
+        if (!_.isEmpty(data.filter)) {
+            match = {
+                company: data.filter.company
+            }
+
+        } else {
+            match = {}
+
+        }
+        console.log("match---", match);
+        CompanyProduct.find(match)
+            .order(options)
+            .keyword(options)
+            .page(options,
+                function (err, found) {
+                    if (err) {
+                        callback(err, null);
+                    } else if (found) {
+                        console.log("found", found);
+                        callback(null, found);
+                    } else {
+                        callback("Invalid data", null);
+                    }
+                });
+
+    }
 };
 module.exports = _.assign(module.exports, exports, model);
--- backend/js/controllers/controller.js
+++ backend/js/controllers/controller.js
@@ -11,7 +11,7 @@ myApp.controller('DashboardCtrl', function ($scope, TemplateService, NavigationS
         if ($.jStorage.get("accessToken")) {
 
         } else {
-            // $state.go("login");
+            $state.go("login");
         }
     })
 
@@ -352,60 +352,60 @@ myApp.controller('DashboardCtrl', function ($scope, TemplateService, NavigationS
         };
 
 
-    //     $scope.getAllItems = function (keywordChange) {
-    //         console.log("in getAllItems")
-    //         console.log("keywordChange",keywordChange)
-    //         $scope.totalItems = undefined;
-    //         if (keywordChange) {
-    //             $scope.currentPage = 1;
-    //         }
-    //         var filters = _.cloneDeep($scope.search);
-    //   delete filters.keyword;
-    //         NavigationService.search($scope.json.json.apiCall.url, {
-    //                 page: $scope.currentPage,
-    //       keyword: $scope.search.keyword,
-    //       filter: filters
-    //             }, ++i,
-    //             function (data, ini) {
-    //                 console.log("in search");
-    //                 if (ini == i) {
-    //                     console.log("ini is i");
-    //                     $scope.items = data.data.results;
-    //                     $scope.totalItems = data.data.total;
-    //                     $scope.maxRow = data.data.options.count;
-    //                 }
-    //                 else {
-    //           toastr.error("No Data Found ");
-    //         }
-    //             });
-    //     };
+        //     $scope.getAllItems = function (keywordChange) {
+        //         console.log("in getAllItems")
+        //         console.log("keywordChange",keywordChange)
+        //         $scope.totalItems = undefined;
+        //         if (keywordChange) {
+        //             $scope.currentPage = 1;
+        //         }
+        //         var filters = _.cloneDeep($scope.search);
+        //   delete filters.keyword;
+        //         NavigationService.search($scope.json.json.apiCall.url, {
+        //                 page: $scope.currentPage,
+        //       keyword: $scope.search.keyword,
+        //       filter: filters
+        //             }, ++i,
+        //             function (data, ini) {
+        //                 console.log("in search");
+        //                 if (ini == i) {
+        //                     console.log("ini is i");
+        //                     $scope.items = data.data.results;
+        //                     $scope.totalItems = data.data.total;
+        //                     $scope.maxRow = data.data.options.count;
+        //                 }
+        //                 else {
+        //           toastr.error("No Data Found ");
+        //         }
+        //             });
+        //     };
 
 
-$scope.getAllItems = function (keywordChange) {
-      console.log("searchh--------", keywordChange);
-      $scope.totalItems = undefined;
-      if (keywordChange) {
-        $scope.currentPage = 1;
-      }
-      var filters = _.cloneDeep($scope.search);
-      delete filters.keyword;
-      NavigationService.search($scope.json.json.apiCall.url, {
-          page: $scope.currentPage,
-          keyword: $scope.search.keyword,
-          filter: filters
-        }, ++i,
-        function (data, ini) {
-          if (ini == i) {
-            if (data.value == true) {
-              $scope.items = data.data.results;
-              $scope.totalItems = data.data.total;
-              $scope.maxRow = data.data.options.count;
-            } else {
-              toastr.error("No Data Found ");
+        $scope.getAllItems = function (keywordChange) {
+            console.log("searchh--------", keywordChange);
+            $scope.totalItems = undefined;
+            if (keywordChange) {
+                $scope.currentPage = 1;
             }
-          }
-        });
-    };
+            var filters = _.cloneDeep($scope.search);
+            delete filters.keyword;
+            NavigationService.search($scope.json.json.apiCall.url, {
+                    page: $scope.currentPage,
+                    keyword: $scope.search.keyword,
+                    filter: filters
+                }, ++i,
+                function (data, ini) {
+                    if (ini == i) {
+                        if (data.value == true) {
+                            $scope.items = data.data.results;
+                            $scope.totalItems = data.data.total;
+                            $scope.maxRow = data.data.options.count;
+                        } else {
+                            toastr.error("No Data Found ");
+                        }
+                    }
+                });
+        };
 
 
         JsonService.refreshView = $scope.getAllItems;
@@ -421,20 +421,20 @@ $scope.getAllItems = function (keywordChange) {
             });
         };
 
-console.log($stateParams.id);
+        console.log($stateParams.id);
 
-        if($stateParams.id == 'viewCompanyProduct'){
-            NavigationService.CompanyProductSearch(function(data){
-               console.log('...............',data); 
-               $scope.allProduct = data.data.results;
+        if ($stateParams.id == 'viewCompanyProduct') {
+            NavigationService.CompanyProductSearch(function (data) {
+                console.log('...............', data);
+                $scope.allProduct = data.data.results;
             });
         };
 
 
-        if($stateParams.id == 'viewCompanyCategory'){
-            NavigationService.companySearch(function(data){
-               console.log('...............',data); 
-               $scope.allCategory = data.data.results;
+        if ($stateParams.id == 'viewCompanyCategory') {
+            NavigationService.companySearch(function (data) {
+                console.log('...............', data);
+                $scope.allCategory = data.data.results;
             });
         };
     })
@@ -670,7 +670,7 @@ console.log($stateParams.id);
         TemplateService.title = $scope.menutitle;
         $scope.template = TemplateService;
         $scope.currentHost = window.location.origin;
-        Console.log("stateParams1",stateParams.id)
+
         if ($stateParams.id) {
             if ($stateParams.id === "AccessNotAvailable") {
                 toastr.error("You do not have access for the Backend.");
@@ -687,14 +687,14 @@ console.log($stateParams.id);
             NavigationService.removeAccessToken();
         };
 
-//for User Login//
-// $scope.userData = data.data;
-// console.log("userdata",$scope.userData);
-//  if (!_.isEmpty($scope.userData)) {
-//             $scope.User = {};
-//             $scope.User.company = $scope.userData.company;
-//             $.jStorage.set("User", $scope.User);
-//           }
+        //for User Login//
+        // $scope.userData = data.data;
+        // console.log("userdata",$scope.userData);
+        //  if (!_.isEmpty($scope.userData)) {
+        //             $scope.User = {};
+        //             $scope.User.company = $scope.userData.company;
+        //             $.jStorage.set("User", $scope.User);
+        //           }
     })
 
     .controller('CountryCtrl', function ($scope, TemplateService, NavigationService, $timeout, $state, $stateParams, toastr) {
